2fcb8b6a8db8ba0f8692479930f6bae0f31caf3a
Robert Desjardins <roberttdesjardins@gmail.com>
2018-04-11 15:20:26 -0700

    Environemnt and part of player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake() {
+		floorMask = LayerMask.GetMask("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		
+	}
+}
