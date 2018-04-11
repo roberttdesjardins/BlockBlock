224c15bb01b2dc44004d045d8da169889718acc8
Robert Desjardins <roberttdesjardins@gmail.com>
2018-04-11 15:47:52 -0700

    Finished player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -17,6 +17,40 @@ public class PlayerMovement : MonoBehaviour
 	}
 
 	void FixedUpdate() {
-		
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+		Animating (h, v);
+	}
+
+	void Move (float h, float v) {
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	void Animating(float h, float v) {
+		print ("h is" + h);
+		print ("v is" + v);
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking);
 	}
 }
